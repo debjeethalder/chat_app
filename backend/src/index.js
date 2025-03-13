05fe1b072a0fd8018d005e92a5a181eb1f12fbbe
debjeethalder <your_github_email@example.com>
2025-03-14 00:25:51 +0530

    save

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,13 +33,10 @@ app.use("/api/messages", messageRoutes);
 
 // Serve Frontend in Production Mode
 if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(__dirname, "../frontend/dist");
-    console.log("Serving frontend from:", frontendPath);
-    
-    app.use(express.static(frontendPath));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendPath, "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 
